6c5e687a308d8adba6b33199cf131b80d19d1386
pathexplorer <77250594+pathexplorer@users.noreply.github.com>
2025-11-04 15:59:49 +0200

    add class

--- gcp_actions/blob_manipulation.py
+++ gcp_actions/blob_manipulation.py
@@ -2,19 +2,25 @@ import os
 import json
 from typing import Any
 from gcp_actions.client import get_bucket
+import logging
+
+# Get the root logger (which GCF has already configured)
+logger = logging.getLogger()
+# Set its level directly
+logger.setLevel(logging.DEBUG)
 
 bucket = get_bucket()
 
-def upload_blob_from_file(
+def upload_to_gcp_bucket(
         gcs_path: str,
-        local_path: Any,
+        local_path: Any | None = None,
         filetype: str = ""
 ) -> None:
     """
     :param gcs_path: folder/filename.extension, on Storage, gs://
     :param local_path: /folder/filename, on virtual machine
-    :param filetype:
-    :return: filename or string (json)
+    :param filetype: "filename" or "string" (json)
+    :return:
     """
     if not gcs_path:
         raise ValueError("GCS path must not be empty")
@@ -34,15 +40,15 @@ def upload_blob_from_file(
             raise RuntimeError(f"Failed to upload {local_path} to {gcs_path}: {e}")
 
 
-def download_blob_if_exists(
+def download_from_gcp_bucket(
         blob_name: str,
         local_path: str | None = None,
         filetype: str = ""
 ) -> bool | Any | None:
     """
     :param blob_name: folder/filename.extension, on Storage, gs://
-    :param local_path: /folder/filename, on virtual machine
-    :param filetype: blob or text
+    :param local_path: /folder/filename, on virtual machine, ONLY for blob, not text
+    :param filetype: "blob" (download_to_filename) or "text" (download_to_text).
     :return:
     """
     if not blob_name:
@@ -54,13 +60,13 @@ def download_blob_if_exists(
         if filetype == "blob":
             return False
         elif filetype == "text":
+            logging.info("Create empty text blob")
             return {}
     # 2. Handling Filetypes
     if filetype == "blob":
         if not local_path:
             # Re-introduce the mandatory check for 'blob' download
             raise ValueError("Local path must not be empty for filetype 'blob'")
-
         # Create folder if it doesn't exist
         os.makedirs(os.path.dirname(local_path), exist_ok=True)
         try:
